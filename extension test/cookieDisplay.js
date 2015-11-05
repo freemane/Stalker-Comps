712024515001e993f20d634aefe2fb07abd20d43
krossar <krossar@cmc307-08.mathcs.carleton.edu>
2015-11-04 20:04:19 -0600

    Have cookie info display in pop from clicking extension

--- extension test/cookieDisplay.js
+++ extension test/cookieDisplay.js
@@ -1,9 +1,8 @@
-
-chrome.browserAction.onClicked.addListener(function(activeTab){
-        chrome.cookies.getAll({},function(cookies) {
-
+//chrome.browserAction.onClicked.addListener(function(activeTab){
+//    alert("before");
+    chrome.cookies.getAll({},function(cookies) {
         var cook = cookies[0];
-        alert("Name: " + cook.name + "\nValue: " + cook.value + "\nDomain: " + cook.domain);
+        $(".cookie").append("<p>Name: " + cook.name + "\nValue: " + cook.value + "\nDomain: " + cook.domain + "</p>");
     });
-    
-});
+
+//});
