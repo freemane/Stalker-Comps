35927d1d6f639e8ec1d98fd9d505de1c9be3ef81
Cody <bohlmanc@carleton.edu>
2015-11-09 20:26:47 -0600

    added cookie counter, still removes cookies

--- extension test/cookieDisplay.js
+++ extension test/cookieDisplay.js
@@ -1,5 +1,3 @@
-//chrome.browserAction.onClicked.addListener(function(activeTab){
-//    alert("before");
 chrome.cookies.getAll({}, function (cookies) {
     console.log('testing');
     for (var i = 0; i < cookies.length; i++) {
@@ -9,37 +7,52 @@ chrome.cookies.getAll({}, function (cookies) {
         //}
 
     }
+    $(".count").append("<p>Num cookies: " + cookies.length + "</p>");
     return;
 });
 
 
 function removeCookies() {
-    //alert('hi');
     chrome.cookies.getAll({},
         function (cookies) {
             var startNum = cookies.length;
             for (var j = 0; j < cookies.length; j++) {
                 var cookie = cookies[j];
 
-                //if (cookie.domain.charAt(0) != ".") {
-                console.log(cookie.domain);
-                chrome.cookies.remove({
-                    url: "http://" + cookie.domain,
-                    name: cookie.name
-                });
-                chrome.cookies.remove({
-                    url: "https://" + cookie.domain,
-                    name: cookie.name
-                });
-                chrome.cookies.remove({
-                    url: cookie.domain,
-                    name: cookie.name
-                });
-                //}
+                if (cookie.domain.charAt(0) != ".") {
+                    console.log(cookie.domain);
+                    chrome.cookies.remove({
+                        url: "http://" + cookie.domain,
+                        name: cookie.name
+                    });
+                    chrome.cookies.remove({
+                        url: "https://" + cookie.domain,
+                        name: cookie.name
+                    });
+                    chrome.cookies.remove({
+                        url: cookie.domain,
+                        name: cookie.name
+                    });
+                } else {
+                    console.log(cookie.domain);
+                    cookie.domain = cookie.domain.substring(1, cookie.domain.length)
+                    chrome.cookies.remove({
+                        url: "http://" + cookie.domain,
+                        name: cookie.name
+                    });
+                    chrome.cookies.remove({
+                        url: "https://" + cookie.domain,
+                        name: cookie.name
+                    });
+                    chrome.cookies.remove({
+                        url: cookie.domain,
+                        name: cookie.name
+                    });
+                }
 
             }
             var endNum = cookies.length - startNum;
-            //console.log(endNum);
+            console.log(endNum);
             //alert(endNum + " cookies deleted.");
         });
     location.reload();
