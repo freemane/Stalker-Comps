fc327423d9c2a71c45337e80504966f8e44a9a7d
Robert Krossa <krossar@carleton.edu>
2016-01-06 21:18:35 -0600

    Updated db so now the object for each cookie contains an array of domains that have accessed/changed the cookie

--- CookieJar/background.js
+++ CookieJar/background.js
@@ -18,29 +18,68 @@
 //  chrome.tabs.create({ url: newURL });
 //});
     
-var curUrl = undefined;
-var curUrlVisitTime = undefined;
 
-chrome.history.onVisited.addListener( function(result) {
-    curUrl = result.url;
-    curUrlVisitTime = result.lastVisitTime;
 
-});
+function getCurrentTab(callback) {
+    chrome.tabs.query(
+        {
+            active: true,
+            lastFocusedWindow: true
+        }, function (tabs) {
+            callback(tabs[0]);
+        });
+}
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
                                      
 chrome.cookies.onChanged.addListener ( function (changed) {
     var cookie = changed.cookie;
     var cause = changed.OnChangedCause;
     if (!changed.removed) {
-    	var cookieInfo = {sourceUrl:curUrl,modTime:curUrlVisitTime};
-        var key = cookie.domain.concat(cookie.name);
-        var setObject = {};
-        setObject[key] = cookieInfo;
-    	chrome.storage.local.set(setObject, function (){
-    		//return function on storage of cookie success
-    		chrome.storage.local.get(key,function (item) {
-                var info = item[key];
+        getCurrentTab( function (tab) {
+            var key = cookie.domain.concat(cookie.name);
+            chrome.storage.local.get()
+        	var domain = extractDomain(tab.url);
+            setDomainInfo(key,domain,function () {
+                //idk
             });
-    	});
         // alert("new cookie!: " + cookie.name+ "domain: "+ cookie.domain + "\n set from: "+curUrl);
     }
 });
+
+
+function setDomainInfo(key,domain,callback) {
+    chrome.storage.local.get(key,function(item){
+        var setObject = item[key];
+        if (typeof setObject != 'object') {
+            var t = new Date();
+            setObject= {domains:[],setTime:t.getTime(),count:0};
+        }
+        setObject[domains].push(domain);
+        setObject[count]++;
+        chrome.storage.local.set(item, function () {
+            //verify??
+        });
+        callback;
+
+    });
+}
+
+function handleError(lastError) {
+    if (lastError){
+        console.log(lastError.message)
+    }
+}
