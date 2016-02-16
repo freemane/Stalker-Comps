cf1f9972a7b6bbf222762e3e141ddd2de8213e92
freemane <emma@snapdocs.com>
2016-02-16 13:56:53 -0600

    if statement in cookieDisplay

--- CookieJar/js/cookieDisplay.js
+++ CookieJar/js/cookieDisplay.js
@@ -437,6 +437,9 @@ function initializeTabs() {
         if (id == selectedId) {
             tabLinks[id].className = 'selected';
         } else {
+            if (id == 'graph'){
+                contentDivs[id].className = 'tabContent hideGraph';
+            }
             contentDivs[id].className = 'tabContent hide';
         }
         i++;
