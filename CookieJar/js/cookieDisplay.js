0d81c46b761b49d63eb4e5e3820235f2a1ecf81b
bohlmanc <bohlmanc@carleton.edu>
2016-02-20 18:42:02 -0600

    Made the container scrollable

--- CookieJar/js/cookieDisplay.js
+++ CookieJar/js/cookieDisplay.js
@@ -350,7 +350,9 @@ Allows for shift clicking to select multiple rows at once
 //TODO - Modularize the shiftClick and show functionality into different functions
 function initializeDataTable(tableName, lengthOption) {
     cookieTable = $('#' + tableName).DataTable({
-      paging: false
+      paging: false,
+      scrollY:        "300px",
+      scrollCollapse: true
         // 'lengthMenu': lengthOption
     });
     $('#'+tableName).css({"overflow":"scroll !important","height":"450px !important"});
